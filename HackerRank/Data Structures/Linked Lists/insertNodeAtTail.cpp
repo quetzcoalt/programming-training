1447b1e05735c46cebc8cde045dd6af3089b7be6
quetzcoalt <quetzcoalt19@gmail.com>
2026-06-27 21:27:36 +0000

    Update insertNodeAtTail.cpp

--- HackerRank/Data Structures/Linked Lists/insertNodeAtTail.cpp
+++ HackerRank/Data Structures/Linked Lists/insertNodeAtTail.cpp
@@ -1,22 +1,61 @@
-/*
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode* next;
- * };
- *
- */
-SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
-    SinglyLinkedListNode *newNode = new SinglyLinkedListNode(data);
-    newNode->next = NULL;
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+        Node (int d) {
+            data=d;
+            next=NULL;
+        }
+};
+
+class Solution {
+    public:
+        Node* insertNodeAtTail(Node* head, int data) {
+            Node *newNode = new Node(data);
+            newNode->next = NULL;
+            
+            Node *current = head;
+            
+            if (head == NULL) return newNode;
+            else {
+                while (current->next) current = current->next;
+                
+                current->next = newNode;
+            }
+            
+            return head;
+        }
+
+        void display(Node *head)
+        {
+            Node *start=head;
+            
+            while (start) {
+                cout << start->data << " ";
+                start = start->next; 
+            }
+        }
+};
+
+int main() {
+    Node* head=NULL;
     
-    SinglyLinkedListNode *current = head;
+    Solution myList;
     
-    if (head == NULL) return newNode;
-    else {
-        while (current->next) current = current->next;
-        
-        current->next = newNode;
+    int T, data;
+    cin >> T;
+
+    while (T-->0) {
+        cin >> data;
+        head=myList.insertNodeAtHead(head, data);
     }
-    
-    return head;
+
+    myList.display(head);
+
+    return 0;
 }
+
