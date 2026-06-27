6344d3790f2dc5306d6949f385919cd53ee6be43
quetzcoalt <quetzcoalt19@gmail.com>
2026-06-27 21:28:21 +0000

    Update printLinkedList.cpp

--- HackerRank/Data Structures/Linked Lists/printLinkedList.cpp
+++ HackerRank/Data Structures/Linked Lists/printLinkedList.cpp
@@ -1,15 +1,26 @@
-/*
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode* next;
- * };
- *
- */
-void printLinkedList(SinglyLinkedListNode* head) {
-    SinglyLinkedListNode *start = head;
-    
-    while (start) {
-        cout << start->data << endl;
-        start = start->next;
-    }
-}
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+        Node (int d) {
+            data=d;
+            next=NULL;
+        }
+};
+
+class Solution {
+    public:
+        void printLinkedList(Node *head)
+        {
+            Node *start=head;
+            
+            while (start) {
+                cout << start->data << " ";
+                start = start->next; 
+            }
+        }
+};
